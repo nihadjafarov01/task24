03b8f30d909c06182ae671a795bbdc966945c01c
nihadjafarov01 <mi72kv3d0@code.edu.az>
2023-10-24 09:16:37 +0400

    first commit

---
+++ ConsoleApp1/Models/Company.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    internal class Company
+    {
+        string _name;
+        string[] _employeesArray;
+
+        public Company(string name)
+        {
+            _name = name;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleApp1.Models;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee user = new Employee("Nihad","Jafarov",20);
+            Console.WriteLine(user.Username);
+            
+        }
+    }
+}
